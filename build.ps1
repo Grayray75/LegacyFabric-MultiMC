9fc17fc8992ad239f674a04fa27da4de56f676e6
Grayray75 <69988482+Grayray75@users.noreply.github.com>
2022-01-25 19:00:08 +0100

    Update build.ps1

--- build.ps1
+++ build.ps1
@@ -1,17 +1,19 @@
+# This script compresses the contents of the directories "legacy_fabric_*" 
+# and creates the corresponding zip files in "dist".
+# In order for this script to run, 7zip must be added to the system path.
+
 $outputFolder = "dist"
 $mainFolder = [IO.Directory]::GetCurrentDirectory()
 
-
 $dirs = [IO.Directory]::GetDirectories($mainFolder, "legacy_fabric_*")
-foreach($dir in $dirs)
-{
-    echo "" "-----------------------------------------------------------------------"
-    cd $dir
+foreach ($dir in $dirs) {
+    Write-Output "" "-------------------------------------------------------------------------"
+    Set-Location $dir
 
     $dirName = [IO.Path]::GetFileName($dir)
     $zipPath = [IO.Path]::Combine($mainFolder, $outputFolder, ($dirName + ".zip"))
     7z a -tzip $zipPath *
-    cd ..
+    Set-Location ..
 }
 
-echo "" "Finished"
+Write-Output "" "Finished"
